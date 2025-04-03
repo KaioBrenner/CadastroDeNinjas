7728cc0d23bb65c1a822eb4f0ae0969a90d06458
KaioBrenner <kaiobrenner@pop-os.intelbras.local>
2025-04-03 11:23:21 -0300

    feat: Coloca o Lombok na Classe para abstração

--- src/main/java/com/kaiobrenner/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/com/kaiobrenner/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -2,9 +2,17 @@ package com.kaiobrenner.CadastroDeNinjas.Missoes;
 
 import com.kaiobrenner.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Entity
 @Table(name="tb_missoes")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MissoesModel {
 
     @Id
@@ -13,49 +21,8 @@ public class MissoesModel {
     private String nome;
     private String dificuldade;
     @OneToMany(mappedBy = "missoes")
-    private NinjaModel ninja;
+    private List<NinjaModel> ninjas;
 
 
-    public MissoesModel() {
-    }
 
-    public MissoesModel(Long id, String nome, String dificuldade, NinjaModel ninja) {
-        this.id = id;
-        this.nome = nome;
-        this.dificuldade = dificuldade;
-        this.ninja = ninja;
-    }
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getDificuldade() {
-        return dificuldade;
-    }
-
-    public void setDificuldade(String dificuldade) {
-        this.dificuldade = dificuldade;
-    }
-
-    public NinjaModel getNinja() {
-        return ninja;
-    }
-
-    public void setNinja(NinjaModel ninja) {
-        this.ninja = ninja;
-    }
 }
--- src/main/java/com/kaiobrenner/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/com/kaiobrenner/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -1,10 +1,17 @@
 package com.kaiobrenner.CadastroDeNinjas.Ninjas;
 
 
+import com.kaiobrenner.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tb_cadastro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaModel {
 
     @Id
@@ -14,39 +21,8 @@ public class NinjaModel {
     private String email;
     private int idade;
     @ManyToOne
-    @JoinColumn(name = "missao_id")
-    private String missao;
+    @JoinColumn(name = "missoes_id")
+    private MissoesModel missoes;
+    private String corDoOlho;
 
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public NinjaModel() {
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
