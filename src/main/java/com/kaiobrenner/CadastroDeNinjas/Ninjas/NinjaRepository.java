4ec7be5eb8bf27da2d73055d786896505056c70e
KaioBrenner <kaiobrenner@pop-os.intelbras.local>
2025-04-04 09:22:21 -0300

    feat: adiciona jpa ao ninja repository

--- src/main/java/com/kaiobrenner/CadastroDeNinjas/Ninjas/NinjaRepository.java
+++ src/main/java/com/kaiobrenner/CadastroDeNinjas/Ninjas/NinjaRepository.java
@@ -1,4 +1,6 @@
 package com.kaiobrenner.CadastroDeNinjas.Ninjas;
 
-public interface NinjaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NinjaRepository extends JpaRepository<NinjaModel, Long> {
 }
